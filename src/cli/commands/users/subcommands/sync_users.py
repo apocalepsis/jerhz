1380944147293ec931d0d5b6607d15fe72faefeb
Flores <alejandro.x.flores@gmail.com>
2017-10-29 10:16:08 -0600

    0

--- src/cli/commands/users/subcommands/sync_users.py
+++ src/cli/commands/users/subcommands/sync_users.py
@@ -1,6 +1,5 @@
 import sys
 import os
-import re
 
 from subprocess import Popen, PIPE, STDOUT
 from config import properties
@@ -23,50 +22,80 @@ def run(args):
 
     for user in user_list:
 
-        print("Validating environment for user <{}>".format(user.get_username()))
+        print("Checking user <{}> status".format(user.get_username()))
 
-        result = shell.run(["id","-u",user.get_username()])
+        user_exists = False
+
+        result = shell.run(["getent","passwd",user.get_username()])
         print(result)
         if result["return_code"] != 0:
-            print("[ERROR] Unable to validate environment.")
-            sys.exit(1)
-
-        m = re.compile("^\d*$")
-        if m.match(result["out"]):
-            print("User <{}> already deployed".format(user.get_username()))
+            print("[ERROR] Unable to check user status.")
             continue
+        elif result["out"]:
+            user_exists = True
 
-        print("Creating user <{}>".format(user.get_username()))
-        result = shell.run(["useradd",user.get_username()])
-        print(result)
-        if result["return_code"] != 0:
-            print("[ERROR] Unable to create user.")
-            continue
-
-        user_password = user.get_password()
-        if user_password:
-            print("Creating password for user <{}>".format(user.get_username()))
-            user_password = cipher.aes.decrypt(user_password).decode("utf-8")
-            result = shell.run(["passwd",user_password])
+        if not user_exists:
+            print("Creating user <{}>".format(user.get_username()))
+            result = shell.run(["useradd",user.get_username()])
             print(result)
-            user_password = None
             if result["return_code"] != 0:
-                print("[ERROR] Unable to create user password.")
+                print("[ERROR] Unable to create user.")
+                continue
+            elif result["out"]:
                 continue
 
-        print("Assigning uid <{}> to user <{}>".format(user.get_uid(),user.get_username()))
-        result = shell.run(["usermod","-u",str(user.get_uid()),user.get_username()])
-        print(result)
-        if result["return_code"] != 0:
-            print("[ERROR] Unable to assign uid.")
-            continue
+        if not user_exists:
+            user_password = user.get_password()
+            if user_password:
+                print("Creating password for user <{}>".format(user.get_username()))
+                user_password = cipher.aes.decrypt(user_password).decode("utf-8")
+                result = shell.run(["passwd",user_password])
+                print(result)
+                user_password = None
+                if result["return_code"] != 0:
+                    print("[ERROR] Unable to create user password.")
+                    continue
+                elif result["out"]:
+                    continue
 
-        print("Assigning gid <{}> to user <{}>".format(user.get_gid(),user.get_username()))
-        result = shell.run(["groupmod","-g",str(user.get_gid()),user.get_username()])
-        print(result)
-        if result["return_code"] != 0:
-            print("[ERROR] Unable to assign gid.")
-            continue
+        if not user_exists:
+            print("Assigning uid <{}> to user <{}>".format(user.get_uid(),user.get_username()))
+            result = shell.run(["usermod","-u",str(user.get_uid()),user.get_username()])
+            print(result)
+            if result["return_code"] != 0:
+                print("[ERROR] Unable to assign uid.")
+                continue
+            elif result["out"]:
+                continue
+
+        if not user_exists:
+
+            group_exists = False
+
+            result = shell.run(["getent","group",str(user.get_gid())])
+            print(result)
+            if result["return_code"] != 0:
+                print("[ERROR] Unable to check group status.")
+                continue
+            elif result["out"]:
+                group_exists = True
+
+            print("Assigning gid <{}> to user <{}>".format(user.get_gid(),user.get_username()))
+            if not group_exists:
+                print("Group not exists, creating it.")
+                result = shell.run(["groupadd",str(user.get_username()),"-g",str(user.get_gid())])
+                print(result)
+                if result["return_code"] != 0:
+                    print("[ERROR] Unable to create ggroup.")
+                    continue
+
+            if group_exists:
+                print("Group exists, adding user.")
+                result = shell.run(["usermod","-g",str(user.get_gid()),user.get_username()])
+                print(result)
+                if result["return_code"] != 0:
+                    print("[ERROR] Unable to assign gid to user.")
+                    continue
 
         print("")
 

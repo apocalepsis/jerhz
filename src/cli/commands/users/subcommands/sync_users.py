53caea64cd818fdaa06d6538eeac691eb89c3e92
Flores <alejandro.x.flores@gmail.com>
2017-10-29 09:19:43 -0600

    0

--- src/cli/commands/users/subcommands/sync_users.py
+++ src/cli/commands/users/subcommands/sync_users.py
@@ -5,6 +5,7 @@ import re
 from subprocess import Popen, PIPE, STDOUT
 from config import properties
 from lib.utils import shell
+from lib.utils import cipher
 from lib.users.dao.linux import DAO as LinuxDAO
 
 def run(args):
@@ -22,7 +23,7 @@ def run(args):
 
     for user in user_list:
 
-        print("    Validating environment for user <{}> ...".format(user.get_username()))
+        print("    Validating environment for user <{}>\n".format(user.get_username()))
 
         result = shell.run(["id","-u",user.get_username()])
         if result["return_code"] != 0:
@@ -32,13 +33,23 @@ def run(args):
         m = re.compile("^\d*$")
         if m.match(result["out"]):
             print("        User <{}> already deployed".format(user.get_username()))
-            return
+            continue
 
-        print("    Creating user <{}> ...".format(user.get_username()))
-        result = shell.run(["useradd",user.get_username()])
+        print("    Creating user <{}>\n".format(user.get_username()))
+        result = shell.run(["useradd","-u",user.get_uid(),"-g",user.get_gid(),user.get_username()])
         if result["return_code"] != 0:
             print("        [ERROR] Unable to create user <{}>".format(user.get_username()))
-            sys.exit(1)
+            continue
+
+        user_password = user.get_password()
+        if user_password:
+            print("    Creating password for user <{}>\n".format(user.get_username()))
+            user_password = cipher.aes.decrypt(user_password).decode("utf-8")
+            print(user_password)
+            result = shell.run(["passwd",user_password])
+            if result["return_code"] != 0:
+                print("        [ERROR] Unable to create user <{}>".format(user.get_username()))
+                continue
 
 
     print(">>> Done.")

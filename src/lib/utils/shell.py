27b806868d2263f43ceb33621ca373b6ca505452
Flores <alejandro.x.flores@gmail.com>
2017-11-09 12:06:19 -0600

    0

--- src/lib/utils/shell.py
+++ src/lib/utils/shell.py
@@ -10,7 +10,8 @@ def run(cmd,pshell=False,cwdir=None):
 
     p = Popen(cmd,shell=pshell,stdout=PIPE, stdin=PIPE, stderr=STDOUT,cwd=cwdir)
     out, err = p.communicate()
-    print("RETURN CODE: " + str(p.returncode))
+
+    response["status_code"] = p.returncode
 
     if out:
         response["out"] = out.decode("utf-8").strip()

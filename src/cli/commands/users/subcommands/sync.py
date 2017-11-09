563daa7944608c09ec8ed91b156bf344e236bb61
Flores <alejandro.x.flores@gmail.com>
2017-11-09 07:31:00 -0600

    0

--- src/cli/commands/users/subcommands/sync.py
+++ src/cli/commands/users/subcommands/sync.py
@@ -23,7 +23,7 @@ def run(args):
 
         print("User <{}> status".format(user.get_username()))
 
-        user_dir = "{}/{}".format(properties.jerhz_users_dir,user.get_username()))
+        user_dir = "{}/{}".format(properties.jerhz_users_dir,user.get_username())
         print("Checking user dir <{}>".format(user_dir))
         if not os.path.isdir(user_dir):
             print("[ERROR]: Dir not found or invalid")

7b84f33b3b9efccef53c1a97b5432fbfb92af850
Flores <alejandro.x.flores@gmail.com>
2017-11-09 07:19:00 -0600

    0

--- src/cli/commands/users/subcommands/sync.py
+++ src/cli/commands/users/subcommands/sync.py
@@ -8,10 +8,12 @@ def run(args):
 
     print(">>> Sync in progress, please wait ... \n")
 
-    print("Checking jerhz users dir <{}>\n".format(properties.jerhz_users_dir))
+    print("Checking jerhz users dir <{}>".format(properties.jerhz_users_dir))
     if not os.path.isdir(properties.jerhz_users_dir):
         print("[ERROR]: Dir not found or invalid")
         sys.exit(1)
+
+    print("")
 
     user_dao_linux = LinuxDAO()
 
